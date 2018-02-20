55b9dc04a3b30ff3fe361c80162e90fec8028515
Anirban Goswami <417799@cognizant.com>
2018-02-20 09:27:27 +0530

    DBContext setup, DTO, VWModel cls setup

--- CmsShoppingCart/App_Start/BundleConfig.cs
+++ CmsShoppingCart/App_Start/BundleConfig.cs
@@ -13,6 +13,11 @@ namespace CmsShoppingCart
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
+            //admin section
+            bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
+                      "~/Scripts/jquery-{version}.js"
+                      ,"~/Scripts/bootstrap.js"));
+
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
